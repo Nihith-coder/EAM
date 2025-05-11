a45d850288fc434290b3dc3a97409586e0d1674d
Nihith Kopparapu <nihith.kopparapu@in.abb.com>
2025-05-11 12:32:05 +0530

    `Update creds.service.ts: add return type annotations and whitespace`

--- src/creds/creds.service.ts
+++ src/creds/creds.service.ts
@@ -3,7 +3,10 @@ import * as k8s from '@kubernetes/client-node';
 
 @Injectable({})
 export class credsService {
-  loadconfig(): { kc: k8s.KubeConfig; k8sApi: k8s.CoreV1Api } {
+  loadconfig(): {
+    kc: k8s.KubeConfig;
+    k8sApi: k8s.CoreV1Api;
+  } {
     const kc = new k8s.KubeConfig();
     kc.loadFromDefault(); // or loadFromCluster()
 
@@ -13,8 +16,9 @@ export class credsService {
       console.log(kubeConfig[0]);
     }
 
-    const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
-    return { kc, k8sApi};
+    const k8sApi = kc.makeApiClient(
+      k8s.CoreV1Api,
+    );
+    return { kc, k8sApi };
   }
-
 }
